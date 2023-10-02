c03b63444b27826f8d5c09b61b04bca819dd4df1
Aarya Paradkar <aaru7276@gmail.com>
2023-10-02 15:40:21 +0530

    firebase configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKcldAEZTYBhL7ne-Oo4xGUCrkpLiZPzA",
+  authDomain: "prodo-cc27b.firebaseapp.com",
+  projectId: "prodo-cc27b",
+  storageBucket: "prodo-cc27b.appspot.com",
+  messagingSenderId: "99307923792",
+  appId: "1:99307923792:web:9e9976bae4bd0cc99a6455",
+  measurementId: "G-9ZF7EMNTQC"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
